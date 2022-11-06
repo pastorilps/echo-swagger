eccaff07b4ed4eac10447d920a938555204051c0
PastoriLps <rafael211295@gmail.com>
2022-11-06 13:16:56 -0300

    .

--- app/docs/docs.go
+++ app/docs/docs.go
@@ -25,62 +25,62 @@ const docTemplate = `{
     "host": "{{.Host}}",
     "basePath": "{{.BasePath}}",
     "paths": {
-        "/users/{userId}": {
-          "get": {
-            "tags": [
-              "Get User By Id"
-            ],
-            "summary": "Find user by ID",
-            "description": "Returns a single user",
-            "operationId": "getUserById",
-            "parameters": [
-              {
-                "name": "userId",
-                "in": "path",
-                "description": "ID of user to return",
-                "required": true,
-                "schema": {
-                  "type": "integer",
-                  "format": "int16"
-                }
-              }
-            ],
-            "responses": {
-              "200": {
-                "description": "successful operation"
-              },
-              "400": {
-                "description": "Invalid ID supplied"
-              },
-              "404": {
-                "description": "User not found"
-              }
-            }
-          }
-        },
         "/users": {
-          "get": {
+            "get": {
             "tags": [
-              "Get Users"
+                "Get Users"
             ],
             "summary": "Find users",
             "description": "Returns multiple users",
             "operationId": "getUsers",
             "parameters": [],
             "responses": {
-              "200": {
+                "200": {
                 "description": "successful operation"
-              },
-              "400": {
+                },
+                "400": {
                 "description": "Invalid ID supplied"
-              },
-              "404": {
+                },
+                "404": {
                 "description": "Users not found"
-              }
+                }
             }
-          }
+            }
+        },
+        "/users/{userId}": {
+            "get": {
+            "tags": [
+                "Get User By Id"
+            ],
+            "summary": "Find user by ID",
+            "description": "Returns a single user",
+            "operationId": "getUserById",
+            "parameters": [
+                {
+                "name": "userId",
+                "in": "path",
+                "description": "ID of user to return",
+                "required": true,
+                "schema": {
+                    "type": "integer",
+                    "format": "int16"
+                }
+                }
+            ],
+            "responses": {
+                "200": {
+                "description": "successful operation"
+                },
+                "400": {
+                "description": "Invalid ID supplied"
+                },
+                "404": {
+                "description": "User not found"
+                }
+            }
+            }
         }
-      }
+    }
 }`
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
